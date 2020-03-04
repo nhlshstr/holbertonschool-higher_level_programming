3c858d4fc5c9aa8ebb87abdebeee430421900595
Nehal Shastri <nhlshstr@gmail.com>
2020-03-03 22:07:04 -0800

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 --linked
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows 
 INNER JOIN  tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDR BY title, genre.id ASC;
+ORDER BY title, genre.id ASC;
