c5d4c56773e4bbbe3135b643d99ececbf4933d5c
Nehal Shastri <nhlshstr@gmail.com>
2020-03-03 21:00:02 -0800

    syntax

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,2 @@
 -- Lists all cities in database
-USE cities, states 
-SELECT cities.id, cities.name, states.name
-FROM cities 
-INNER JOIN states
-ON cities.state_id = states.id;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id;
