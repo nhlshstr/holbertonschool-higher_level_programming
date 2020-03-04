260b425ec0401b25955599b8794beff6b6f8a11d
Nehal Shastri <nhlshstr@gmail.com>
2020-03-03 17:33:10 -0800

    privilages granted

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Creates user if not exists with password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
