c9d8e83adb0aef7f13e1c29f3c479ab18d8f3cce
Nehal Shastri <nhlshstr@gmail.com>
2020-03-03 20:54:39 -0800

    initial

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,6 @@
 -- Lists all cities in database
-SELECT cities.id, cities.name, states.name ORDER BY cities.id ASC;
+USE cities, states 
+SELECT cities.id, cities.name, states.name
+FROM cities 
+INNER JOIN states
+ON cities.state_id = states.id;
