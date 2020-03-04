9fc95d068a5072c6fcad1b48a2f9940e556bcf33
Nehal Shastri <nhlshstr@gmail.com>
2020-03-03 22:04:27 -0800

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
 -- tv_show.title, tv_show_genres.genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genre.show_id
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY title, genre_id ASC;
