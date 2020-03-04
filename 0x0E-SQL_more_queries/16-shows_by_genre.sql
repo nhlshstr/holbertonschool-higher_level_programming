8aaff4c9d141df5b19b8b8744f44570f57765109
Nehal Shastri <nhlshstr@gmail.com>
2020-03-03 22:32:13 -0800

    changed join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 --Lists all shows, and all genres linked to that show, from the database
-SELECT tv_shows.title, tv_genres.name FROM tv_show_genres
-RIGHT OUTER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-LEFT OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
