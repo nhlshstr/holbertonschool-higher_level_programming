fb1c66d80ce44d4f25a2e6fda7fd17b03403513d
Nehal Shastri <nhlshstr@gmail.com>
2020-03-30 00:29:22 -0700

    changed syntax in same spot

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-'''
-Lists all states in a table that matches argv[4]
-'''
+"""
+Script that displays all values in the states
+table where name matches the argument
+"""
+
 if __name__ == "__main__":
-    import sys
     import MySQLdb
-    
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], )
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name=%s", format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", (argv[4], ))
     states = cur.fetchall()
     for state in states:
-        print((state))
+        print(state)
