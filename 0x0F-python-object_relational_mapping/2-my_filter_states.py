83c2938da2466f3d304caeae775801e95b3b8e95
Nehal Shastri <nhlshstr@gmail.com>
2020-03-30 00:11:24 -0700

    added task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#Lists all states in a table that matches argv[4]
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(sys.argv[4]))
+    states = cur.fetchall()
+    for state in states:
+        print((state))
