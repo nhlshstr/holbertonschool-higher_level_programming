b6774371a82946c24b20a695490f7212f04e56c0
Nehal Shastri <nhlshstr@gmail.com>
2020-03-30 00:13:00 -0700

    added task 2 with different comment style

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-#Lists all states in a table that matches argv[4]
-
+'''
+Lists all states in a table that matches argv[4]
+'''
 if __name__ == "__main__":
     import sys
     import MySQLdb
