2df925573c55919f34d317c4bc12e4f559586953
Nehal Shastri <nhlshstr@gmail.com>
2020-03-29 23:52:45 -0700

    added task 2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#Lists all states in a table that start with 'N'
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id")
+    states = cur.fetchall()
+    for state in states:
+        if (state[1][0] == "N"):
+            print((state[0], state[1]))
