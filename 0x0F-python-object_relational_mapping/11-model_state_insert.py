386e44aae3a060583fc30ee3d313e851857e2043
root <nhlshstr@gmail.com>
2020-03-31 00:35:07 +0000

    added task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""lists all the states in the table"""
+
+if __name__ == "__main__":
+    from model_state import State, Base
+    from sqlalchemy import (create_engine, Table, Integer,
+                            String, Column)
+    from sqlalchemy.orm import sessionmaker
+    import sys
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    sesh = sessionmaker()
+    sesh.configure(bind=engine)
+    session__ = sesh()
+
+    to_add = State(name="Loisiana")
+    session__.add(to_add)
+    session__.flush()
+    print(to_add.id)
+    session__.commit()
