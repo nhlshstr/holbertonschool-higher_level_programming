28c5753d4fb94206a3fe7598785fac6a9b379d5a
Nehal Shastri <nhlshstr@gmail.com>
2020-05-06 00:15:02 -0700

    added task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function secondBiggest (arr) {
+  let max = -Infinity;
+  let max2 = -Infinity;
+  for (let x = 0; x < arr.length; x++) {
+    if (arr[x] > max) {
+      max2 = max;
+      max = arr[x];
+    } else if (max > arr[x] > max2) {
+      max2 = arr[x];
+    }
+  }
+  return max2;
+}
+
+const arr = process.argv.slice(2, process.argv.length).map(Number);
+console.log(secondBiggest(arr));
