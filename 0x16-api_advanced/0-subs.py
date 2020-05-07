4610ed91306e0e0a7b519a81300e60e45d8a8cf6
Nehal Shastri <nhlshstr@gmail.com>
2020-05-07 11:27:28 -0700

    added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """ Returns total subs for subreddit """
+import requests
 
 
-if __name__ == "__main__":
-    import requests
-
-    def number_of_subscribers(subreddit):
-        r = requests.get(
-            'https://www.reddit.com/r/{}/about.json'.format(subreddit),
-            headers={
-                'User-agent': 'NehalShastri'
-            }).json()
-        return (0 if ('message' in r and
-                      r['message'] == 'Not Found') else
-                r['data']['subscribers'])
+def number_of_subscribers(subreddit):
+    r = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit),
+        headers={
+            'User-agent': 'NehalShastri'
+        }).json()
+    return (0 if ('message' in r and
+                  r['message'] == 'Not Found') else
+            r['data']['subscribers'])
