a6c890029cf0909b1349ef93d3ff919464074ba1
Nehal Shastri <nhlshstr@gmail.com>
2020-05-12 13:32:45 -0700

    added 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Reads & prints filei
+
+const fs = require('fs');
+
+fs.readFile('cisfun', 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
