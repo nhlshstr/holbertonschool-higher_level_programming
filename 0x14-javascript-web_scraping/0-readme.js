cab4b14ec0eb4ed660e04d04c96a9705c84e2a41
Nehal Shastri <nhlshstr@gmail.com>
2020-05-12 13:39:24 -0700

    added argv[2]

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 
-fs.readFile('cisfun', 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
     return;
