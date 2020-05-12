972dbbaed4d1b1ad3e60bb4bd78029e3721b8a21
Nehal Shastri <nhlshstr@gmail.com>
2020-05-12 14:25:28 -0700

    added 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,2 +1,11 @@
-#!/usr/env/node
+#!/usr/bin/node
 // Writes a string to a file
+
+const fs = require('fs');
+
+const data = process.argv[3];
+const file = process.argv[2];
+
+fs.writeFile(file, data, (err) => {
+  if (err) throw err;
+});
