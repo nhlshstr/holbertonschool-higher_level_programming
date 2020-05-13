37222b04ee3c9f6f6eb57c2d50a78ca9a0d48e48
Nehal Shastri <nhlshstr@gmail.com>
2020-05-12 19:26:36 -0700

    changed arg for fs

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 req(process.argv[2], (err, res, body) => {
   if (err) throw err;
-  fs.writeFile('loripsum', JSON.parse(body), 'utf-8', error => {
+  fs.writeFile(process.argv[3], JSON.parse(body), 'utf-8', error => {
     if (error) throw error;
   });
 });
